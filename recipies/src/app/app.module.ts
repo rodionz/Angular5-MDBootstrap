a8d399104205d1c29497aa2dc8a88792096cb841
Rodion Zinovyev <rodionz@012.net.il>
2018-01-15 22:26:31 +0200

    navbar

--- recipies/src/app/app.module.ts
+++ recipies/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
