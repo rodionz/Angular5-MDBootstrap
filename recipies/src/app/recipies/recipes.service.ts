d8d1499da83029f6f773deeaa786de9118ba7e56
Rodion Zinovyev <rodionz@012.net.il>
2018-01-30 20:31:17 +0200

    services

--- recipies/src/app/recipies/recipes.service.ts
+++ recipies/src/app/recipies/recipes.service.ts
@@ -1,12 +1,25 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Recipe', 'This is another simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe', 
+    'This is simply a test', 
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+  [
+    new Ingredient('Meat',1),
+    new Ingredient('French Fries', 20)
+
+  ]),
+    new Recipe('Another Recipe',
+     'This is another simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    [new Ingredient('Buns',2),
+    new Ingredient('Meat',2)]
+  )
   ];
   constructor() { }
    
