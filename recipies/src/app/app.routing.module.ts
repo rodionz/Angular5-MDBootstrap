c0a88483ab95beff287444cc773c8dcdc6ff5153
Odiki <rodion.zinovyev@gmail.com>
2018-01-31 15:11:39 +0200

    enough

--- recipies/src/app/app.routing.module.ts
+++ recipies/src/app/app.routing.module.ts
@@ -4,15 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping list/shopping-list.component';
 import { RecipesComponent } from './recipies/recipes.component';
 import { RecipeStartComponent } from './recipies/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent},
-    { path: 'new', component: RecipeEditComponent },
+    //{ path: 'new', component: RecipeEditComponent },
     { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent },
+    //{ path: ':id/edit', component: RecipeEditComponent },
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
--- recipies/src/app/recipies/recipe-detail/recipe-detail.component.ts
+++ recipies/src/app/recipies/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
+import { ActivatedRoute } from '@angular/router/src/router_state';
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,10 +10,12 @@ import { RecipesService } from '../recipes.service';
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-  constructor(private recipeService : RecipesService) { }
+  recipe: Recipe;
+  constructor(private recipeService : RecipesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+    .subscribe();
   }
 
   onAddToShoppingList(){
